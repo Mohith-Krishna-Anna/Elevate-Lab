02653657091c944f129f2336ffc934d00531e2a9
mohith krishna <mohithkrishna703@gmail.com>
2025-06-24 19:58:19 +0530

    Add files via upload

---
+++ Task-2/task2.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
+ds = pd.read_csv("C:/Users/mohit/Downloads/Titanic-Dataset.csv")
+
+
+print("summary statistics:\n",ds.describe(include = 'all'))
+
+# histograms
+numeric_features = ['Age', 'SibSp', 'Parch', 'Fare']
+ds[numeric_features].hist(bins=20, figsize=(12, 6), color='skyblue')
+plt.tight_layout()
+plt.show()
+
+
+for col in numeric_features:
+    sns.boxplot(x=ds[col], color='lightcoral')
+    plt.title(f"Boxplot of {col}")
+    plt.tight_layout()
+    plt.show()
+
+ds_encoded = ds.copy()
+label_encoder = LabelEncoder()
+ds_encoded['Sex'] = label_encoder.fit_transform(ds['Sex'])
+ds_encoded['Embarked'] = label_encoder.fit_transform(ds['Embarked'])
+
+ 
+sns.pairplot(ds[['Survived', 'Pclass', 'Age', 'Sex', 'Fare']], hue='Survived')
+plt.show()
+
+
+sns.barplot(x='Sex', y='Survived', data=ds)
+plt.title("Survival Rate by Passenger Class")
+plt.ylabel("Survival Probability")
+plt.show()
+print(" Survival Rate by Age:",ds.groupby('Sex')['Survived'].mean())
+
+
+sns.barplot(x='Pclass', y='Survived', data=ds)
+plt.title("Survival Rate by Passenger Class")
+plt.ylabel("Survival Probability")
+plt.show()
+print(" Survival Rate by Age:",ds.groupby('Pclass')['Survived'].mean())
+
+sns.barplot(x='Age', y='Survived', data=ds)
+plt.title("Survival Rate by Passenger Class")
+plt.ylabel("Survival Probability")
+plt.show()
+print(" Survival Rate by Age:",ds.groupby('Age')['Survived'].mean())
